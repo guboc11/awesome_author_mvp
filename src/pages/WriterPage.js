48099b940a5db978af7ef7d11f033343f44f89ce
guboc11 <guboc11@gmail.com>
2024-07-08 20:51:23 +0900

    Add writer page structure lines

--- src/pages/WriterPage.js
+++ src/pages/WriterPage.js
@@ -2,8 +2,45 @@ import '../App.css'
 
 export default function WriterPage() {
   return(
-    <div>
-      <p>writer</p>
+    <div className="w-[1024px] mx-auto">
+
+      <hr className="border-4 my-3 border-indigo-600"></hr>
+
+      <div className="border-4 border-red-500 p-10 flex">
+        <div className="border-4 border-yellow-500 w-3/5">
+          <div className="border-4 border-green-500">
+            <div className="border-4 border-blue-500 h-16"></div>
+            <div className="border-4 border-blue-500 h-32"></div>
+            <div className="border-4 border-blue-500 h-20"></div>
+            <div className="border-4 border-blue-500 h-20"></div>
+            <div className="border-4 border-blue-500 h-20"></div>
+          </div>
+          <div className="border-4 border-green-500">
+            <div className="border-4 border-blue-500 h-20"></div>
+            <div className="border-4 border-blue-500 h-96"></div>
+          </div>
+        </div>
+        <div className="border-4 border-yellow-500 w-2/5 h-fit">
+          <div className="border-4 border-green-500">
+            <div className="border-4 border-blue-500 h-32"></div>
+            <div className="border-4 border-blue-500 h-20"></div>
+            <div className="border-4 border-blue-500 h-20"></div>
+            <div className="border-4 border-blue-500 h-20"></div>
+            <div className="border-4 border-blue-500 h-20"></div>
+          </div>
+          <div className="border-4 border-green-500">
+            <div className="border-4 border-blue-500 h-48"></div>
+          </div>
+        </div>
+      </div>
+
+      <div className="border-4 border-red-500 p-10">
+        <div className="border-4 border-yellow-500 h-20">
+        </div>
+        <div className="border-4 border-yellow-500 h-96">
+        </div>
+      </div>
+      
     </div>
   )
 }
