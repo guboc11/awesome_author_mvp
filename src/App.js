6eea39e01fa787b281fb8eeaa5e47f4760b4058c
guboc11 <guboc11@gmail.com>
2024-07-08 17:15:37 +0900

    Update routers & Add MyPage layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import MainPage from './pages/MainPage';
 import WriterPage from './pages/WriterPage';
 import EmployerPage from './pages/EmployerPage';
 import LoginPage from './pages/LoginPage';
+import MyPageNav from './components/MyPageNav';
 
 export default function App() {
   return(
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />}>
-          {/* <Route index element={<Home />} /> */}
+        <Route path="/" element={<MainPage />}> </Route>
+        <Route path="/mypage" element={<MyPageNav />}>
           <Route path="writer" element={<WriterPage />} />
           <Route path="employer" element={<EmployerPage />} />
-          <Route path="login" element={<LoginPage />} />
-          {/* <Route path="*" element={<NoPage />} /> */}
         </Route>
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   )
