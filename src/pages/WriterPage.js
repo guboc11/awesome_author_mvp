dd4218b099176d4ea350084c47b8533e6b99b5b6
guboc11 <guboc11@gmail.com>
2024-07-09 15:54:51 +0900

    Update section1 of writer page : padding, margin, icon etc

--- src/pages/WriterPage.js
+++ src/pages/WriterPage.js
@@ -1,4 +1,12 @@
 import '../App.css'
+import CodeIcon from '@mui/icons-material/Code';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import ShareIcon from '@mui/icons-material/Share';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import { grey } from '@mui/material/colors';
 
 export default function WriterPage() {
   return(
@@ -11,91 +19,98 @@ export default function WriterPage() {
         <div className="w-3/5">
           <div className="">
             <div className="flex justify-between">
-              <div className="ml-5 flex gap-1">
-                <p>아이콘</p>
-                <p>개발</p>
-                <p>아이콘</p>
-                <p>2년</p>
+              <div className="flex gap-2">
+                <CodeIcon sx={{backgroundColor: grey[200], borderRadius: 2, fontSize: 30, my: "auto", p: "4px"}}/>
+                <p className="my-auto">개발</p>
+                <AccessTimeIcon sx={{backgroundColor: grey[200], borderRadius: 2, fontSize: 30, my: "auto", p: "4px"}}/>
+                <p className="my-auto">2년</p>
               </div>
               <div>
-                <p>3일전</p>
+                <p className="text-gray-500 text-sm">3일전</p>
               </div>
             </div>
-            <div className="flex justify-between items-center">
+            <div className="my-10 flex justify-between items-center">
               <div className="flex items-center gap-3">
-                <img className="w-20 h-auto rounded-full" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"/>
+                <img className="w-24 h-auto rounded-full" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"/>
                 <div>
                   <p className="text-4xl font-bold">Kenoz</p>
-                  <p>대한민국</p>
+                  <div className="flex">
+                    <LocationOnIcon />
+                    <p>대한민국</p>
+                  </div>
                 </div>
               </div>
-              <div className="flex gap-3">
-                <p className="text-2xl">공유</p>
-                <p className="text-2xl">저장</p>
+              <div className="flex gap-2">
+                <button>
+                  <ShareIcon sx={{border: 1, borderRadius: 10, fontSize: 40, p: "10px"}}/>
+                </button>
+                <button>
+                  <BookmarkIcon sx={{border: 1, borderRadius: 10, fontSize: 40, p: "10px"}}/>
+                </button>
               </div>
             </div>
             <div className="h-20">
               <p className="text-xl font-bold">CJ 엔터테인먼트 드라마 작가 3년차, 웹드라마 '너와나' 대본 작가. 네이버 웹소설 공모전 우수상. 경희대 문예창작학과 졸업</p>
             </div>
             <div className="flex flex-wrap gap-2 ">
-              <p className="p-1 bg-gray-100 rounded-full">#DataScience</p>
-              <p className="p-1 bg-gray-100 rounded-full">#Flutter</p>
-              <p className="p-1 bg-gray-100 rounded-full">#FastApi</p>
-              <p className="p-1 bg-gray-100 rounded-full">#Python</p>
-              <p className="p-1 bg-gray-100 rounded-full">#C/C++</p>
-              <p className="p-1 bg-gray-100 rounded-full">#DeepLearning</p>
-              <p className="p-1 bg-gray-100 rounded-full">#AI</p>
-              <p className="p-1 bg-gray-100 rounded-full">#ComputerVision</p>
+              <p className="py-1 px-3 bg-gray-100 rounded-full">#DataScience</p>
+              <p className="py-1 px-3 bg-gray-100 rounded-full">#Flutter</p>
+              <p className="py-1 px-3 bg-gray-100 rounded-full">#FastApi</p>
+              <p className="py-1 px-3 bg-gray-100 rounded-full">#Python</p>
+              <p className="py-1 px-3 bg-gray-100 rounded-full">#C/C++</p>
+              <p className="py-1 px-3 bg-gray-100 rounded-full">#DeepLearning</p>
+              <p className="py-1 px-3 bg-gray-100 rounded-full">#AI</p>
+              <p className="py-1 px-3 bg-gray-100 rounded-full">#ComputerVision</p>
             </div>
-            <div className="flex py-3 items-center gap-3">
-              <button className="p-5 bg-blue-500 text-white rounded-full flex gap-3">
-                <p>매일 아이콘</p>
+            <div className="flex my-5 items-center gap-3">
+              <button className="py-2 px-3 bg-blue-500 text-white rounded-full flex gap-3">
+                <EmailOutlinedIcon />
                 <p>매일</p>
               </button>
-              <button className="p-5 bg-blue-500 text-white rounded-full flex gap-3">
-                <p>메세지 아이콘</p>
+              <button className="py-2 px-3 bg-blue-500 text-white rounded-full flex gap-3">
+                <ChatBubbleOutlineIcon />
                 <p>메세지 </p>
               </button>
             </div>
           </div>
           <div className="">
-            <div className="py-10">
+            <div className="py-10 -mb-2">
               <p className="text-3xl font-bold">경력</p>
             </div>
             <div className="">
               <div className="flex gap-5">
-                <div className='p-3'>
+                <div className='px-3 py-2'>
                   <div className="bg-blue-600 w-3 h-3 rounded-full"></div>
                   <div className="border-2 border-blue-500 w-0 h-full m-auto my-1"></div>
                 </div>
-                <div>
-                  <div className="text-sm bg-blue-600 text-white py-1 px-2 rounded-full">2023.04.05-2023.09.07</div>
+                <div className="pb-5">
+                  <div className="text-sm bg-blue-600 text-white py-1 px-3 rounded-full">2023.04.05-2023.09.07</div>
                   <div className="text-lg font-bold">SBS 방송국 | 대한민국</div>
-                  <div className="text-lg font-bold text-gray-300">R&D | CTO</div>
-                  <div>개발 총괄</div>
+                  <div className="font-bold text-gray-300">R&D | CTO</div>
+                  <div className="font-bold">개발 총괄</div>
                 </div>
               </div>
-              <div className="flex gap-5">
-                <div className='p-3'>
+              <div className="flex gap-5 my-1">
+                <div className='px-3 py-2'>
                   <div className="bg-blue-600 w-3 h-3 rounded-full"></div>
                   <div className="border-2 border-blue-500 w-0 h-full m-auto my-1"></div>
                 </div>
-                <div>
-                  <div className="text-sm bg-blue-600 text-white py-1 px-2 rounded-full">2023.11.08-2024.01.30</div>
+                <div className="pb-5">
+                  <div className="text-sm bg-blue-600 text-white py-1 px-3 rounded-full">2023.11.08-2024.01.30</div>
                   <div className="text-lg font-bold">JTBC 드라마 '영혼' | 대한민국</div>
-                  <div className="text-lg font-bold text-gray-300">개발 | 데이터사이언티스트</div>
-                  <div>데이터 분석</div>
+                  <div className="font-bold text-gray-300">개발 | 데이터사이언티스트</div>
+                  <div className="font-bold">데이터 분석</div>
                 </div>
               </div>
-              <div className="flex gap-5">
-                <div className='p-3'>
+              <div className="flex gap-5 my-1">
+                <div className='px-3 py-2'>
                   <div className="bg-blue-600 w-3 h-3 rounded-full"></div>
                 </div>
-                <div>
-                  <div className="text-sm bg-blue-600 text-white py-1 px-2 rounded-full">2024.03.01-2024.07.01</div>
+                <div className="pb-5">
+                  <div className="text-sm bg-blue-600 text-white py-1 px-3 rounded-full">2024.03.01-2024.07.01</div>
                   <div className="text-lg font-bold">CJ 엔터 | 대한민국</div>
-                  <div className="text-lg font-bold text-gray-300">개발자 | 개발</div>
-                  <div>개발 총괄</div>
+                  <div className="font-bold text-gray-300">개발자 | 개발</div>
+                  <div className="font-bold">개발 총괄</div>
                 </div>
               </div>
             </div>
